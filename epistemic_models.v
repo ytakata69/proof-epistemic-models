eb7dda89b0cdcee6ac3c000470aed9ac2cd1155b
ytakata69 <takata.yoshiaki@kochi-tech.ac.jp>
2023-10-24 18:20:04 +0900

    Add PredicateOnRp

--- epistemic_models.v
+++ epistemic_models.v
@@ -134,6 +134,25 @@ Proof.
   now apply Htra with (w':=w').
 Qed.
 
+(* E is a predicate on W and E w depends only on R p w *)
+Definition PredicateOnRp (p : P) (E : Ensemble W) : Prop :=
+  forall (w w' : W),
+  R p w = R p w' ->
+    (In _ E w <-> In _ E w').
+
+Lemma PredicateOnRp_transitivity :
+  forall (p : P) (E : Ensemble W),
+    PredicateOnRp p E ->
+  forall (w1 w2 : W),
+    In _ (R p w1) w2 ->
+    (In _ E w1 <-> In _ E w2).
+Proof.
+  intros p E Hpred w1 w2 H12.
+  unfold PredicateOnRp in Hpred.
+  apply Hpred.
+  now apply same_R_p.
+Qed.
+
 End KripkeFrames.
 
 Section BeliefOperators.
@@ -170,126 +189,155 @@ Inductive D (H : Ensemble P) (E : Ensemble W) : Ensemble W :=
 
 Hypothesis K_is_KD45 : isKD45 K.
 
-Lemma CB_is_included_by_pow_B :
-  forall (p : P) (E : Ensemble W) (n : nat),
-  n > 0 ->
-  Included _ (CB (Singleton _ p) E) (fpow (B p) n E).
+Lemma Rplus_singleton_equals_R_p :
+  forall (p : P) (w : W),
+  Rplus (Singleton _ p) w = R p w.
 Proof.
-  intros p E n Hnpos.
-  destruct n as [| m].
-  - (* when n = 0 *)
-  inversion Hnpos.
-
-  - (* when n = S m *)
-  clear Hnpos.
-  induction m as [| m IHm];
+  intros p w.
+  apply Extensionality_Ensembles.
+  unfold Same_set.
+  split;
   unfold Included;
-  intros x HCBx;
-  inversion HCBx as [w' HRPx EQw'];
-  clear w' EQw' HCBx;
-  unfold Included in HRPx;
-  unfold fpow;
-  apply B_intro;
-  unfold Included;
-  intros y HRxy.
-  + (* when m = 0 *)
-  apply HRPx.
-  apply Rplus_base with (H:=Singleton _ p) in HRxy;
-  [apply HRxy | apply In_singleton].
-  + (* when m > 0 *)
-  unfold Included in IHm.
-  apply IHm.
-  apply CB_intro.
-  unfold Included.
-  intros z HRPz.
-  apply HRPx.
-  apply Rplus_trans with (p:=p) (w2:=y);
-  [apply In_singleton | apply HRxy | apply HRPz].
-Qed.
-
-Definition posnat (n : nat) : Prop := n > 0.
-
-Lemma CB_is_included_by_intersection_of_pow_B :
-  forall (p : P) (E : Ensemble W),
-  Included _ (CB (Singleton _ p) E)
-    (IntersectionForall posnat (fun n => fpow (B p) n E)).
-Proof.
-  intros p E.
-  unfold Included.
-  intros x HCBx.
-  apply IntersectionForall_intro.
-  intros n Hn.
-  destruct Hn as [| m];
-  generalize dependent x;
-  apply CB_is_included_by_pow_B;
-  apply Nat.lt_0_succ.
-Qed.
-
-Lemma CB_includes_intersection_of_pow_B :
-  forall (p : P) (E : Ensemble W),
-  Included _
-    (IntersectionForall posnat (fun n => fpow (B p) n E))
-    (CB (Singleton _ p) E).
-Proof.
-  intros p E.
-  unfold Included.
-  intros x HIBx.
-  inversion HIBx as [x' HIBx' EQx'];
-  clear x' EQx' HIBx.
-
-  apply CB_intro.
-  unfold Included.
-  intros y HRPxy.
-  generalize dependent HIBx'.
-  induction HRPxy as [p' w' Hpp' x' HRw'x'
-    | p' w' x' y' Hpp' HRw'x' HRPx'y' IH];
+  intros x Hx.
+  - (* to show ⊆ *)
+  induction Hx as [p' w' Hpp' x Hx
+    | p' w' x y Hpp' Hx Hxy IH];
   inversion Hpp' as [EQp];
-  rewrite <- EQp in HRw'x';
+  rewrite <- EQp in Hx;
   rewrite <- EQp;
-  clear p' EQp Hpp';
-  intros HIBw'.
-
-  - (* when Rplus_base *)
-  assert (Hpos1 : In _ posnat 1).
-  { unfold In, posnat. apply Nat.lt_0_succ. }
-  specialize (HIBw' 1 Hpos1);
-  clear Hpos1;
-  unfold fpow in HIBw'.
-  inversion HIBw' as [w Hw' EQw];
-  clear w EQw HIBw'.
-  now apply Hw'.
-  - (* when Rplus_trans *)
-  apply IH.
-  intros n Hn.
-  specialize (HIBw' n Hn).
-  destruct n as [| m].
-  + (* when n = 0 *)
-  inversion Hn.
-  + (* when n = S m *)
-  clear Hn IH HRPx'y'.
+  clear p' EQp Hpp'.
+  + (* when Rplus_base *)
+  apply Hx.
+  + (* when Rplus_trans *)
   destruct K_is_KD45 as [_ [Htra _]].
   unfold transitive in Htra.
-
-  inversion HIBw' as [w Hw' EQw];
-  clear w EQw HIBw'.
-  apply B_intro.
-  unfold Included.
-  intros z HRx'z.
-  apply Hw'.
-  now apply Htra with (w':=x').
+  now apply Htra with (w':=x).
+  - (* to show ⊇ *)
+  generalize dependent x.
+  apply Rplus_base.
+  apply In_singleton.
 Qed.
 
-Lemma CB_equals_intersection_of_pow_B :
+Lemma CB_p_equals_B_p :
   forall (p : P) (E : Ensemble W),
-  CB (Singleton _ p) E =
-    IntersectionForall posnat (fun n => fpow (B p) n E).
+  CB (Singleton _ p) E = B p E.
 Proof.
   intros p E.
   apply Extensionality_Ensembles.
   unfold Same_set.
+  split;
+  unfold Included;
+  intros w Hw.
+  - (* to show ⊆ *)
+  inversion Hw as [w' HRP EQw'];
+  clear w' EQw' Hw.
+  rewrite Rplus_singleton_equals_R_p in HRP.
+  now apply B_intro.
+  - (* to show ⊇ *)
+  inversion Hw as [w' Hw' EQw'];
+  clear w' EQw' Hw.
+  apply CB_intro.
+  now rewrite Rplus_singleton_equals_R_p.
+Qed.
+
+Lemma B_p_is_idempotent :
+  forall (p : P) (E : Ensemble W),
+  B p E = B p (B p E).
+Proof.
+  destruct K_is_KD45 as [Hser [Htra Heuc]].
+
+  intros p E.
+  apply Extensionality_Ensembles.
+  unfold Same_set.
+  split;
+  unfold Included;
+  intros x Hx;
+  inversion Hx as [x' Hx' EQx'];
+  clear x' EQx' Hx.
+  - (* to show ⊆ *)
+  unfold transitive in Htra.
+
+  apply B_intro.
+  unfold Included.
+  intros y Hy.
+  apply B_intro.
+  unfold Included.
+  intros z Hz.
+  apply Hx'.
+  now apply Htra with (w':=y).
+
+  - (* to show ⊇ *)
+  unfold serial in Hser.
+  unfold euclidean in Heuc.
+  destruct (Hser p x) as [x' Hxx'].
+  specialize (Hx' _ Hxx').
+  specialize (Heuc _ _ _ Hxx').
+
+  apply B_intro.
+  unfold Included.
+  intros y Hy.
+  inversion Hx' as [x'' Hx'E EQx''];
+  clear x'' EQx'' Hx'.
+  apply Hx'E.
+  now apply Heuc.
+Qed.
+
+Lemma B_p_equals_pow_B_p :
+  forall (p : P) (E : Ensemble W) (n : nat),
+  n > 0 -> B p E = fpow (B p) n E.
+Proof.
+  intros p E n Hn.
+  destruct n as [| m];
+  [inversion Hn |];
+  clear Hn.
+  induction m as [| m IHm].
+  - (* when m = 0 *)
+  unfold fpow.
+  reflexivity.
+  - (* when m > 0 *)
+  remember (S m) as n eqn:EQn.
+  unfold fpow.
+  unfold fpow in IHm.
+  rewrite <- IHm.
+  apply B_p_is_idempotent.
+Qed.
+
+Theorem PredicateOnRp_is_fixpoint_of_B :
+  forall (p : P) (E : Ensemble W),
+  PredicateOnRp _ p E -> E = B p E.
+Proof.
+  intros p E Hpred.
+  unfold PredicateOnRp in Hpred.
+  apply Extensionality_Ensembles.
+  unfold Same_set.
   split.
-  - apply CB_is_included_by_intersection_of_pow_B.
-  - apply CB_includes_intersection_of_pow_B.
+  - (* to show E ⊆ B p E *)
+  unfold Included.
+  intros w Hi.
+  apply B_intro.
+  unfold Included.
+  intros x Hix.
+  specialize (Hpred w x).
+  apply same_R_p in Hix;
+  [| apply K_is_KD45].
+  now apply (Hpred Hix).
+  - (* to show B p E ⊆ E *)
+  unfold Included.
+  intros w HiB.
+  inversion HiB as [w' Hi EQw'];
+  clear w' EQw' HiB.
+
+  destruct K_is_KD45 as [Hser _].
+  unfold serial in Hser.
+  specialize (Hser p w).
+  destruct Hser as [w' Hw'].
+
+  unfold Included in Hi.
+  specialize (Hi w' Hw').
+  apply same_R_p in Hw';
+  [| apply K_is_KD45].
+
+  now apply (Hpred _ _ Hw').
 Qed.
 
 End BeliefOperators.
@@ -310,35 +358,67 @@ Definition isEpistemicModel (K : Frame P) (sigma : StrProfMap K) : Prop :=
 Definition update (s : StrProf) (p : P) (sp : strategy) : StrProf :=
   fun q => if p =? q then sp else s q.
 
-(*
 Variable win : StrProf -> P -> Prop.
-*)
 Variable K : Frame P.
 Variable sigma : StrProfMap K.
 
-(*
 Definition keepWinning (p : P) (w' : W) (sp' : strategy) : Prop :=
   win (sigma w') p -> win (update (sigma w') p sp') p.
 Definition profitableDev (p : P) (w' : W) (sp' : strategy) : Prop :=
   ~ win (sigma w') p /\ win (update (sigma w') p sp') p.
-*)
-
-Variable somePred : Ensemble W -> Prop.
 
 Definition isRational (p : P) (w : W) : Prop :=
-  somePred (R p w).
-  (*
   forall sp' : strategy,
   (exists w' : W, In _ (R p w) w' /\
     ~ keepWinning p w' sp') \/
   (forall w' : W, In _ (R p w) w' ->
     ~ profitableDev p w' sp').
-  *)
 
 Inductive RAT (p : P) : Ensemble W :=
 | RAT_intro :
     forall w : W, isRational p w
       -> In _ (RAT p) w.
+
+Lemma isRational_is_PredicateOnRp :
+  forall p, PredicateOnRp _ p (isRational p).
+Proof.
+  intros p.
+  unfold PredicateOnRp.
+  intros w w' Heq.
+  split;
+  unfold isRational, In;
+  intros H;
+  intros sp'; specialize (H sp').
+  - (* to show -> *)
+  rewrite <- Heq.
+  destruct H as [H | H];
+  [left | right].
+  + destruct H as [w1 H];
+  now exists w1.
+  + apply H.
+  - (* to show <- *)
+  rewrite Heq.
+  destruct H as [H | H];
+  [left | right].
+  + destruct H as [w1 H];
+  now exists w1.
+  + apply H.
+Qed.
+
+Lemma RAT_is_PredicateOnRp :
+  forall p, PredicateOnRp _ p (RAT p).
+Proof.
+  unfold PredicateOnRp.
+  intros p w w' Heq.
+  apply isRational_is_PredicateOnRp in Heq.
+  unfold In in Heq.
+  split;
+  intros H;
+  apply RAT_intro;
+  inversion H as [x Hr EQx];
+  clear x EQx;
+  now apply Heq.
+Qed.
 
 Hypothesis K_is_KD45 : isKD45 K.
 
@@ -347,84 +427,19 @@ Lemma RAT_transitivity :
   In _ (R p w1) w2 ->
   (In _ (RAT p) w1 <-> In _ (RAT p) w2).
 Proof.
-  destruct K_is_KD45 as [_ [Htra Heuc]].
-  unfold transitive in Htra.
-  unfold Included in Htra.
-  unfold euclidean in Heuc.
-  unfold Included in Heuc.
-
-  intros p w1 w2 H12.
-  apply (same_R_p K_is_KD45) in H12.
-
-  split; intros H1;
-  apply RAT_intro;
-  inversion H1 as [x Hrat EQx];
-  clear x EQx H1;
-  unfold isRational;
-  unfold isRational in Hrat
-  .
-  - now rewrite <- H12.
-  - now rewrite H12.
-
-(*
-  intros sp';
-  specialize (Hrat sp');
-  destruct Hrat as [Hrat | Hrat];
-  [left | right | left | right].
-
-  - (* to show w1 ∈ RAT p -> w2 ∈ RAT p *)
-    (* case 1 of isRational p w2 *)
-  destruct Hrat as [w' [Hi Hrat]].
-  exists w'.
-  split; [| assumption].
-  now apply Heuc with (w:=w1).
-  - (* case 2 of isRational p w2 *)
-  intros w' Hi.
-  apply Hrat.
-  now apply Htra with (w':=w2).
-
-  - (* to show w2 ∈ RAT p -> w1 ∈ RAT p *)
-    (* case 1 of isRational p w1 *)
-  destruct Hrat as [w' [Hi Hrat]].
-  exists w'.
-  split; [| assumption].
-  now apply Htra with (w':=w2).
-  - (* case 2 of isRational p w1 *)
-  intros w' Hi.
-  apply Hrat.
-  now apply Heuc with (w:=w1).
-*)
-
+  intros p w1 w2.
+  apply PredicateOnRp_transitivity;
+  [assumption |].
+  apply RAT_is_PredicateOnRp.
 Qed.
 
 Theorem RAT_is_fixpoint_of_B :
   forall p : P, RAT p = B _ p (RAT p).
 Proof.
   intros p.
-  apply Extensionality_Ensembles.
-  unfold Same_set.
-  split.
-  - (* to show RAT p ⊆ B _ p (RAT p) *)
-  unfold Included.
-  intros w Hi.
-  apply B_intro.
-  unfold Included.
-  intros x Hix.
-  now apply RAT_transitivity with (w1:=w).
-  - (* to show B _ p (RAT p) ⊆ RAT p *)
-  unfold Included.
-  intros w HiB.
-  inversion HiB as [w' Hi EQw'];
-  clear w' EQw' HiB.
-
-  destruct K_is_KD45 as [Hser _].
-  unfold serial in Hser.
-  specialize (Hser p w).
-  destruct Hser as [w' Hw'].
-
-  unfold Included in Hi.
-  specialize (Hi w' Hw').
-  now apply RAT_transitivity with (w2:=w').
+  apply PredicateOnRp_is_fixpoint_of_B;
+  [assumption |].
+  apply RAT_is_PredicateOnRp.
 Qed.
 
 Theorem RAT_equals_pow_B_RAT :
@@ -447,26 +462,9 @@ Theorem RAT_equals_CB_RAT :
     RAT p = CB _ (Singleton _ p) (RAT p).
 Proof.
   intros p.
-  rewrite CB_equals_intersection_of_pow_B;
-  [| apply K_is_KD45].
-  apply Extensionality_Ensembles.
-  unfold Same_set.
-  split.
-  - (* to show RAT p ⊆ ∩_n (B p)^n (RAT p) *)
-  unfold Included.
-  intros x Hx.
-  apply IntersectionForall_intro.
-  intros n Hn.
-  now rewrite <- RAT_equals_pow_B_RAT.
-  - (* to show ∩_n (B p)^n (RAT p) ⊆ RAT p *)
-  unfold Included.
-  intros x Hax.
-  inversion Hax as [x' Hx EQx'];
-  clear x' EQx' Hax.
-  rewrite RAT_equals_pow_B_RAT with (n:=1).
-  apply Hx.
-  unfold In, posnat.
-  apply Nat.lt_0_succ.
+  rewrite CB_p_equals_B_p;
+  [| assumption].
+  apply RAT_is_fixpoint_of_B.
 Qed.
 
 Hypothesis K_sigma_is_EM : isEpistemicModel sigma.
